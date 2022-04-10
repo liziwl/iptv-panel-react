38a6ba904b99351ca56a6b215657e518ddbd7db6
liziwl <leezisy@gmail.com>
2022-04-10 18:23:55 +0800

    加入在线人数

--- src/component/LiveLayout.js
+++ src/component/LiveLayout.js
@@ -5,13 +5,45 @@ import HeaderMenu from "./HeaderMenu"
 import {default as Video} from "./NVideo";
 import Logo from "./Logo";
 import "./LiveLayout.css"
+import axios from "axios";
 
 const {Header, Content} = Layout;
 const {Title} = Typography;
 
 class LiveLayout extends React.Component {
+    state = {
+        online_uv: 0,
+    };
+
+    update_counter() {
+        axios.get('https://iptv.liziwl.cn/status/visitor?vid=covid-test');
+        let data = new FormData();
+        data.append('vid', 'covid-test');
+        axios.post('https://iptv.liziwl.cn/status/online', data)
+            .then(response => {
+                let count = response.data['online_uv'];
+                this.setState({'online_uv': count});
+            });
+    }
+
+
+    componentDidMount() {
+        this.update_counter();
+        this.interval = setInterval(() => {
+            this.update_counter();
+            // 10秒统计一次
+        }, 1000 * 10);
+
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+
     render() {
         const url = "https://live.cra.moe/hls/0/stream.m3u8";
+        const {online_uv} = this.state;
         return (
             <Layout className="live-layout">
                 <Header style={{alignItems: "center", lineHeight: '50px', height: '50px', padding: 0}}>
@@ -33,6 +65,7 @@ class LiveLayout extends React.Component {
                             <Title level={5}>FIND ME ON <a
                                 href="https://sustech.online/"
                                 target="_blank" rel="noreferrer">📚南科手册</a></Title>
+                            <span>当前在线人数：{online_uv}</span>
                             <span>请大家根据现场排队情况选择人员较少时前往。</span>
                             <span>本站仅提供实况直播，不会对画面进行录制和存储，也不会使用程序对直播的画面进行任何分析。</span>
                         </Space>
